32b9be3b9c09da702e8e4ca4f02787a3615386f7
Jennifer Smith <59855990+JenniferSmith007@users.noreply.github.com>
2020-10-04 13:58:48 -0400

    Changed || to && to print correct message

--- conditional.js
+++ conditional.js
@@ -1,16 +1,18 @@
-let mpg = `45`;
+let mpg = `500`;
 if (mpg <= 10) {
   console.log(`it's palpable`);
-} else if (mpg >= 11 || mpg <= 16) {
+} else if (mpg >= 11 && mpg <= 16) {
   console.log(`planet frowns upon this, so does wallet.`);
-} else if (mpg >= 17 || mpg <= 20) {
+} else if (mpg >= 17 && mpg <= 20) {
   console.log(`It's palpable.`);
-} else if (mpg >= 21 || mpg <= 29) {
+} else if (mpg >= 21 && mpg <= 29) {
   console.log(`atmosphere smiles at your decision.`);
-} else if (mpg >= 30 || mpg <= 35) {
+} else if (mpg >= 30 && mpg <= 35) {
   console.log(`Not many gas stops will be taken.`);
-} else if (mpg >= 35 || mpg <= 120) {
+} else if (mpg >= 35 && mpg <= 119) {
   console.log(`the fish of the sea smile at your conservation.`);
-} else if ((mpg = 120)) {
+} else if (mpg == 120) {
   console.log(`if youre using the Tom Ogle fuel system, props`);
+} else {
+  console.log(`Too high`);
 }
